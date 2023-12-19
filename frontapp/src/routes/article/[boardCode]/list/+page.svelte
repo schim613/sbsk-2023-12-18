5d4a5efde7a9db2655457940afe03d573c35b090
schim613 <uximin@naver.com>
2023-12-19 21:25:34 +0900

    slug 이용해서 파일 줄이기

---
+++ frontapp/src/routes/article/[boardCode]/list/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import {page} from '$app/stores';
+    // 페이지 매개변수로부터 boardCode 추출
+    const boardCode = $page.params['boardCode'];
+    // 각 보드 코드에 대한 이름 매핑
+    const boardNames = {
+        notice: '공지사항',
+        free1: '잡담',
+        free2: '방명록'
+    };
+    // 매핑된 이름 가져오기, 매핑되지 않았다면 기본값으로 '게시판' 사용
+    const boardName = ((boardNames[boardCode] || '') + ' 게시판').trim();
+</script>
+
+<h1>{boardName}</h1>
